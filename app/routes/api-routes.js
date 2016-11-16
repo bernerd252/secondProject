49279844832ca74de934bd2bd8a2ee9abd71d50a
Eedouard Batlle <Eedouard Batlle>
2016-11-15 21:03:19 -0500

    images

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,5 +1,5 @@
 var orm 			= require ("../config/orm.js");
-
+var PNGImage = require('pngjs-image');
 
 // Routes
 // =============================================================
@@ -38,5 +38,30 @@ module.exports = function(app){
 		orm.addCleanup(cleanup, function(data){
 		});
 
+		var image = PNGImage.createImage(100, 300);
+ 
+		// Get width and height 
+		console.log(image.getWidth());
+		console.log(image.getHeight());
+		 
+		// Set a pixel at (20, 30) with red, having an alpha value of 100 (half-transparent) 
+		image.setAt(20, 30, { red:255, green:0, blue:0, alpha:100 });
+		 
+		// Get index of coordinate in the image buffer 
+		var index = image.getIndex(20, 30);
+		 
+		// Print the red color value 
+		console.log(image.getRed(index));
+		 
+		// Get low level image object with buffer from the 'pngjs' package 
+		var pngjs = image.getImage();
+		 
+		image.writeImage('/images', function (err) {
+		    if (err) throw err;
+		    console.log('Written to the file');
+		});
+
 	})
+
+	
 }
