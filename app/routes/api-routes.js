592846d092593562441d39c04b47436e786ffe75
gcbest <godfrey.c.best@gmail.com>
2016-11-21 22:10:18 -0500

    removed excess packages

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -4,6 +4,8 @@ var express = require('express');
 var fileUpload = require('express-fileupload');
 var Twit = require('twit');
 var fs = require('fs');
+var passport = require('passport');
+var Strategy = require('passport-twitter').Strategy;
 
 
 // Twitter API
@@ -16,6 +18,24 @@ var T = new Twit({
     access_token_secret: '03RvoJ1rHN7AmgiE6rsecJ4DXVCxIqTfywDmu6fSdbuH9'
 });
 
+
+
+// Initialize Twitter Passport
+// =============================================================
+
+passport.use(new Strategy({
+    consumerKey: process.env.CONSUMER_KEY,
+    consumerSecret: process.env.CONSUMER_SECRET,
+    callbackURL: 'http://127.0.0.1:3000/login/twitter/return'
+  },
+  function(token, tokenSecret, profile, cb) {
+    // In this example, the user's Twitter profile is supplied as the user
+    // record.  In a production-quality application, the Twitter profile should
+    // be associated with a user record in the application's database, which
+    // allows for account linking and authentication with other identity
+    // providers.
+    return cb(null, profile);
+  }));
 
 // Routes
 // =============================================================
